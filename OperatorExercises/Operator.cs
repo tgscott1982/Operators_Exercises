0a8761c89e0af004f7313aefd010e3cb2cb4b468
Travis Gersch <t_gersch@hotmail.com>
2022-07-06 14:18:48 -0500

    operators exercise first commit

---
+++ OperatorExercises/Operator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercises;
+
+    public static class Operator
+    {
+        public static double AreaOfCircle(double radius)
+    {
+            return Math.PI* Math.Pow(radius, 2);
+    }
+
+}
+
---
+++ OperatorExercises/Program.cs
@@ -0,0 +1,41 @@
+﻿#region Exercise 1
+using OperatorExercises;
+int a = 17;
+int b = 4;
+int quotient = a / b;
+int remainder = a % b;
+int sum = a + b;
+int dif = a - b;
+int prod = a * b;
+
+if (a == 17 && b == 4);
+{
+    Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+}
+Console.WriteLine($"The sum is {sum}.");
+Console.WriteLine($"The difference is {dif}.");
+Console.WriteLine($"The product is {prod}.");
+
+
+
+#endregion
+
+
+
+#region Exercise 2
+
+
+Console.WriteLine("\nWhat is the radius of your circle?");
+
+var radius = double.Parse(Console.ReadLine());
+
+double calculatedArea = OperatorExercises.Operator.AreaOfCircle(radius);
+
+Console.WriteLine($"\nThe area of a circle with the radis of {radius} is {calculatedArea}.");
+
+
+
+
+#endregion
+
+
